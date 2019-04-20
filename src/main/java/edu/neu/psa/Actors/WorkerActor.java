4a37568b0e93a74f4cfe278bdaac806de7aca4f5
anubhav1011 <gupta.anu@husky.neu.edu>
2019-04-19 23:43:09 -0400

    Adding function for mutation and crossover

--- src/main/java/edu/neu/psa/Actors/WorkerActor.java
+++ src/main/java/edu/neu/psa/Actors/WorkerActor.java
@@ -30,7 +30,77 @@ public class WorkerActor extends AbstractActor {
             context().stop(getSelf());
 
 
+        }).match(RegenerateGenotype.class, request -> {
+            Population previousGeneration = request.previousGeneration;
+            double crossoverRate = request.crossoverRate;
+            double mutationRate = request.mutationRate;
+            double elitismCount = request.elitismCount;
+            int fitnessIndex = request.fitnessIndex;
+            Database database = request.database;
+            Genotype genotype = crossoverPopulation(previousGeneration, crossoverRate, elitismCount, fitnessIndex);
+            if (fitnessIndex > elitismCount) {
+                mutateGenotype(genotype, database, fitnessIndex, elitismCount, mutationRate);
+            }
+            genotype.createPhenoType(database.getTeams());
+            sender().tell(new MasterActor.Result(genotype), getSelf());
+            context().stop(getSelf());
+
+
         }).build();
+    }
+
+
+    public Genotype selectParent(Population population) {
+        List<Genotype> genotypes = population.getGenotypes();
+        double populationFitness = population.getPopulationFitness();
+        double rouletteWheelPosition = Math.random() * populationFitness;
+        double spinWheel = 0;
+        for (Genotype genotype : genotypes) {
+            spinWheel += genotype.getFitness();
+            if (spinWheel >= rouletteWheelPosition) {
+                return genotype;
+            }
+        }
+        // Get genotypes
+        return genotypes.get(population.populationSize() - 1);
+    }
+
+
+    public Genotype crossoverPopulation(Population previousPopulation, double crossoverRate, double elitismCount, int fitnessIndex) {
+        Genotype parent1 = previousPopulation.getFittest();
+        if (crossoverRate > Math.random() && fitnessIndex >= elitismCount) {
+            Genotype offspring = new Genotype(parent1.getChromosomeLength());
+            Genotype parent2 = selectParent(previousPopulation);
+            for (int geneIndex = 0; geneIndex < parent1.getChromosomeLength(); geneIndex++) {
+                // Use half of parent1's genes and half of parent2's genes
+                if (0.5 > Math.random()) {
+                    offspring.setGene(geneIndex, parent1.getGene(geneIndex));
+                } else {
+                    offspring.setGene(geneIndex, parent2.getGene(geneIndex));
+                }
+            }
+            return offspring;
+
+
+        } else {
+            return parent1;
+        }
+
+    }
+
+    public void mutateGenotype(Genotype genotype, Database database, int fitnessIndex, double elitismCount, double mutationRate) {
+        // Initialize new population
+        //Population newPopulation = new Population();
+        Genotype randomGenotype = new Genotype(database);
+        for (int geneIndex = 0; geneIndex < genotype.getChromosomeLength(); geneIndex++) {
+            // Skip mutation if this is an elite genotype
+            // Does this gene need mutation?
+            if (mutationRate > Math.random()) {
+                // Swap for new gene
+                genotype.setGene(geneIndex, randomGenotype.getGene(geneIndex));
+            }
+        }
+
     }
 
 
@@ -41,6 +111,25 @@ public class WorkerActor extends AbstractActor {
         public CalculateFitness(Genotype genotype, HashMap<Integer, Team> teams) {
             this.genotype = genotype;
             this.teams = teams;
+        }
+    }
+
+    static public class RegenerateGenotype {
+
+        private final int fitnessIndex;
+        private final double crossoverRate;
+        private final Population previousGeneration;
+        private final double mutationRate;
+        private final double elitismCount;
+        private final Database database;
+
+        public RegenerateGenotype(int fitnessIndex, double crossoverRate, Population previousGeneration, double mutationRate, double elitismCount, Database database) {
+            this.fitnessIndex = fitnessIndex;
+            this.crossoverRate = crossoverRate;
+            this.previousGeneration = previousGeneration;
+            this.mutationRate = mutationRate;
+            this.elitismCount = elitismCount;
+            this.database = database;
         }
     }
 }
